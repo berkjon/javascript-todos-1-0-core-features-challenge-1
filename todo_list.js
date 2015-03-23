dc63e01ce40a1feea7c33642000a4b69d20b1316
Jonathan Berk & laomatt <pair+berkjon.laomatt@devbootcamp.com>
2015-03-23 16:31:55 -0700

    completed todo challenge; moving on to grocery list

--- todo_list.js
+++ todo_list.js
@@ -1,35 +1,92 @@
 var newTodoList = function() {
-	this.list = []
+  this.list = []
 };
 
+var taskObject = function(content) {
+  this.content = content;
+  this.completed = false;
+}
+
 newTodoList.prototype.add = function(task) {
-	this.list.push(task);
+  this.list.push(new taskObject(task));
 }
 
 newTodoList.prototype.tasks = function() {
-	for (i in this.list) {
-		console.log(this.list[i]);
-	}
+  for (var i in this.list) {
+    if (this.list[i].completed == true) {
+      console.log("[x] " + this.list[i].content);
+    } else {
+      console.log("[ ] " + this.list[i].content);
+    }
+  }
 }
 
-newTodoList.prototype.remove = function(task) {
-	var index = this.list.indexOf(task);
-	this.list.splice(index, 1);
+
+
+newTodoList.prototype.remove = function(content) {
+  var index = null;
+  var todoList = this;
+  todoList.list.forEach(function(element){
+    if (element.content === task) {
+      index = todoList.list.indexOf(element);
+    };
+  });
+
+  for (var i = 0; i < this.list.length; i++) { 
+  var task = this.list[i];
+    if (task.content === content){
+      this.list.splice(i, 1);
+      return;
+    }
+  }
 }
 
+newTodoList.prototype.complete = function(task) {
+  var index = null;
+  var self = this;
+  self.list.forEach(function(element){
+    if (element.content === task) {
+      index = self.list.indexOf(element);
+    };
+  });
+
+  this.list[index].completed = true
+}
+
+function add_this() {
+  list.list.push(document.getElementById("toAdd").value)
+
+}
 
 // Driver code
 var list = new newTodoList();
 
-list.add('first task')
-list.add('second task')
-list.add('third task')
-list.add('fourth task')
-list.add('fifth task')
+// list.add('first task')
+// list.add('second task')
+// list.add('third task')
+// list.add('fourth task')
+// list.add('fifth task')
 
-list.tasks();
+// list.tasks();
+// console.log("");
+// list.remove('fourth task');
+// console.log("");
 
-list.remove('fourth task');
+// list.tasks(); 
+
+// list.complete('second task')
+// console.log("");
+// list.tasks(); 
+
+// display_list
+// display_list=""
+
+// for(i in list.list){
+//   display_list.concat(list.list[i].content)
+// }
 
 
-list.tasks();
+// document.getElementById('items').innerHTML = display_list +""
+
+
+
