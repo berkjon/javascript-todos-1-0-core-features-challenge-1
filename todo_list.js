a1a95afa9809f81725b2775c5c63dfcb2680f6c9
Jonathan Berk & laomatt <pair+berkjon.laomatt@devbootcamp.com>
2015-03-23 12:03:51 -0700

    challenge work in process

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,35 @@
 var newTodoList = function() {
-	// ???
+	this.list = []
 };
 
+newTodoList.prototype.add = function(task) {
+	this.list.push(task);
+}
+
+newTodoList.prototype.tasks = function() {
+	for (i in this.list) {
+		console.log(this.list[i]);
+	}
+}
+
+newTodoList.prototype.remove = function(task) {
+	var index = this.list.indexOf(task);
+	this.list.splice(index, 1);
+}
 
 
 // Driver code
+var list = new newTodoList();
+
+list.add('first task')
+list.add('second task')
+list.add('third task')
+list.add('fourth task')
+list.add('fifth task')
+
+list.tasks();
+
+list.remove('fourth task');
 
 
-var todoList = newTodoList();
+list.tasks();
